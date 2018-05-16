6d50e592e94cb9c8ab3d481eb6f92dd222b8c9ea
lanhaifeng <lanhaifeng@mchz.com.cn>
2018-05-16 18:38:49 +0800

    添加工具类获取bean

--- src/main/java/com/feng/baseframework/autoconfig/SystemConfiguartion.java
+++ src/main/java/com/feng/baseframework/autoconfig/SystemConfiguartion.java
@@ -1,5 +1,7 @@
 package com.feng.baseframework.autoconfig;
 
+import com.feng.baseframework.util.SpringUtil;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
@@ -17,4 +19,9 @@ import org.springframework.context.annotation.Import;
 @Configuration
 @Import({MybatisConfiguration.class,RedisConfiguration.class,RestTemplateConfiguration.class})
 public class SystemConfiguartion {
+
+    @Bean
+    public SpringUtil getSpringUtil(){
+        return new SpringUtil();
+    }
 }
