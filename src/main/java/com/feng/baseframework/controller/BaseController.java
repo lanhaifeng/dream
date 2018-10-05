b4060a1f53b7d451e5e5c6ddc0b64707447a9358
lanhaifeng <lanhaifeng@mchz.com.cn>
2018-10-05 17:25:51 +0800

    集成drools工具类型，提供动态加载drools规则工具类

--- src/main/java/com/feng/baseframework/controller/BaseController.java
+++ src/main/java/com/feng/baseframework/controller/BaseController.java
@@ -1,8 +1,10 @@
 package com.feng.baseframework.controller;
 
 import com.feng.baseframework.annotation.MethodTimeAop;
+import com.feng.baseframework.model.RuleTp;
 import com.feng.baseframework.model.User;
 import com.feng.baseframework.service.RedisService;
+import com.feng.baseframework.util.DroolsUtil;
 import com.feng.baseframework.util.JacksonUtil;
 import org.apache.log4j.Logger;
 import org.kie.api.runtime.KieSession;
@@ -16,7 +18,9 @@ import org.springframework.web.bind.annotation.RestController;
 import javax.annotation.Resource;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 /**
@@ -74,5 +78,22 @@ public class BaseController {
 
         String response = user == null || user.getUserName() == null ? "" : user.getUserName() + "触发了" + ruleFiredCount + "条规则";
         return  response;
+    }
+
+    @RequestMapping(value = "/drools/dynamicStudent",method={RequestMethod.GET, RequestMethod.POST})
+    public String dynamicLoadRule(@RequestBody User user){
+        KieSession kieSession = DroolsUtil.getInstance().dynamicLoadRule(loadRuleTps());
+        kieSession.insert(user);
+        int ruleFiredCount = kieSession.fireAllRules();
+
+        String response = user == null || user.getUserName() == null ? "" : user.getUserName() + "触发了" + ruleFiredCount + "条动态加载规则";
+        return  response;
+    }
+
+    private List<RuleTp> loadRuleTps(){
+        List<RuleTp> ruleTps = new ArrayList<>();
+        ruleTps.add(new RuleTp("matd : User(userName != null, userName == \"admin\");","System.out.println(\"动态加载rule，admin用户!\");",0));
+        ruleTps.add(new RuleTp("matd : User(userName != null, userName != \"admin\");","System.out.println(\"动态加载rule，非admin用户!\");",1));
+        return  ruleTps;
     }
 }
---
+++ src/main/java/com/feng/baseframework/util/DroolsUtil.java
@@ -0,0 +1,105 @@
+package com.feng.baseframework.util;
+
+import com.feng.baseframework.model.RuleTp;
+import org.drools.core.event.DebugRuleRuntimeEventListener;
+import org.drools.template.ObjectDataCompiler;
+import org.kie.api.KieBase;
+import org.kie.api.KieServices;
+import org.kie.api.builder.KieBuilder;
+import org.kie.api.builder.KieFileSystem;
+import org.kie.api.builder.Message;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
+import org.kie.internal.io.ResourceFactory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.File;
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ * baseframework
+ * 2018/9/30 16:41
+ * drools工具类
+ *
+ * @author lanhaifeng
+ * @since
+ **/
+public class DroolsUtil {
+
+	private static final Logger logger = LoggerFactory.getLogger(DroolsUtil.class);
+
+	private DroolsUtil() {
+	}
+
+	private static class SingletonHolder {
+		static DroolsUtil instance = new DroolsUtil();
+	}
+
+	public static DroolsUtil getInstance() {
+		return SingletonHolder.instance;
+	}
+
+	/**
+	 * 2018/10/5 17:02
+	 * 动态加载drools规则，并返回kieSession
+	 *
+	 * @param ruleTps
+	 * @author lanhaifeng
+	 * @return org.kie.api.runtime.KieSession
+	 */
+	public KieSession dynamicLoadRule(List<RuleTp> ruleTps){
+		try {
+			KieServices kieServices = KieServices.Factory.get();
+			KieFileSystem kfs = kieServices.newKieFileSystem();
+			kfs.write("src/main/resources/dynamic/rules/" + StringUtil.generateUUID() + ".drl", getRulesByTemplate(ruleTps));
+
+			KieBuilder kieBuilder = kieServices.newKieBuilder(kfs).buildAll();
+			if (kieBuilder.getResults().getMessages(Message.Level.ERROR).size() > 0) {
+				throw new RuntimeException(kieBuilder.getResults().getMessages().toString());
+			}
+			KieContainer kieContainer = kieServices.newKieContainer(kieServices.getRepository().getDefaultReleaseId());
+			KieBase kBase = kieContainer.getKieBase();
+
+			KieSession kieSession = kBase.newKieSession();
+			kieSession.addEventListener(new DebugRuleRuntimeEventListener());
+			return kieSession;
+		} catch (Exception e) {
+			logger.error("规则引擎初始化失败，请查看错误信息:" + e.getMessage());
+			throw new RuntimeException(e.getMessage(), e);
+		}
+	}
+
+	/**
+	 * 2018/10/5 16:50
+	 * 根据模板和规则信息生成规则
+	 *
+	 * @param ruleTps
+	 * @author lanhaifeng
+	 * @return java.lang.String
+	 */
+	private String getRulesByTemplate(List<RuleTp> ruleTps) throws Exception {
+		if(ruleTps == null || ruleTps.size() < 1){
+			return null;
+		}
+		ObjectDataCompiler objectDataCompiler = new ObjectDataCompiler();
+
+		String rule = objectDataCompiler.compile(ruleTps, getDroolsTemplate());
+		logger.info(rule);
+
+		return rule;
+	}
+
+	/**
+	 * drools 获取规则模板文件
+	 * @return string
+	 * @throws Exception
+	 */
+	private InputStream getDroolsTemplate() throws Exception{
+		String webRootAppPath = FileUtils.getWebRootPath();
+
+		return ResourceFactory.newFileResource(webRootAppPath+ File.separator
+				+ File.separator + "ruleTemplate/Tp.drl").getInputStream();
+	}
+}
