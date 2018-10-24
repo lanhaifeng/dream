704cbe023bb11f86022f51a8bf75d25ab1bdd7c1
lanhaifeng <lanhaifeng1991@163.com>
2018-10-24 23:02:49 +0800

    添加对象clone的工具类和相关测试

--- src/test/java/com/feng/baseframework/model/Student.java
+++ src/test/java/com/feng/baseframework/model/Student.java
@@ -5,8 +5,35 @@ import lombok.Data;
 import java.io.Serializable;
 
 @Data
-public class Student implements Serializable {
+public class Student implements Serializable, Cloneable {
     private Integer id;
     private String name;
     private Integer age;
+    private Teacher teacher;
+
+    public Student() {
+    }
+
+    public Student(Integer id, String name, Integer age, Teacher teacher) {
+        this.id = id;
+        this.name = name;
+        this.age = age;
+        this.teacher = teacher;
+    }
+
+    //浅拷贝
+    @Override
+    public Student clone() throws CloneNotSupportedException {
+        return (Student)super.clone();
+    }
+
+    //深拷贝
+    /*@Override
+    public Student clone() throws CloneNotSupportedException {
+        Student s = (Student)super.clone();
+        if(s.getTeacher() != null){
+            s.setTeacher(s.getTeacher().clone());
+        }
+        return s;
+    }*/
 }
