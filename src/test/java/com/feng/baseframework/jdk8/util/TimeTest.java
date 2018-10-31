910a76a173ee8e0d65f14411d764136c005a8bb9
lanhaifeng <lanhaifeng@mchz.com.cn>
2018-10-31 19:23:00 +0800

    添加测试

--- src/test/java/com/feng/baseframework/jdk8/util/TimeTest.java
+++ src/test/java/com/feng/baseframework/jdk8/util/TimeTest.java
@@ -1,5 +1,12 @@
 package com.feng.baseframework.jdk8.util;
 
+import com.feng.baseframework.util.DateUtil;
+import org.junit.Test;
+
+import java.util.Date;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 /**
  * @ProjectName: baseframework
  * @Description: 时间api测试
@@ -11,4 +18,32 @@ package com.feng.baseframework.jdk8.util;
  * @Version: 1.0
  */
 public class TimeTest {
+
+	@Test
+	public void getTimeTokenTest(){
+		String[] times = DateUtil.getTimeToken(new Date());
+		System.out.println(times[0]);
+		System.out.println(times[1]);
+		System.out.println(times[2]);
+		System.out.println(times[3]);
+		System.out.println(times[4]);
+		System.out.println(times[5]);
+		System.out.println(times[6]);
+		System.out.println(times[7]);
+		System.out.println(times[3]+times[4]);
+	}
+
+	@Test
+	public void timePatternTest(){
+		String reg = "([2]\\d{3}(((0[13578]|1[02])([0-2]\\d|3[01]))|((0[469]|11)([0-2]\\d|30))|(02([01]\\d|2[0-8])))([01][0-9]|2[0-3])([0-5]\\d)([0-5]\\d))";
+		String str = "9003268435225620181029090237606509";
+		Pattern pattern = Pattern.compile (reg);
+		Matcher matcher = pattern.matcher (str);
+		while (matcher.find ())
+		{
+			System.out.println (matcher.group ());
+		}
+
+		System.out.println(DateUtil.getDateByPattern(str));
+	}
 }
--- src/test/java/com/feng/baseframework/test/StringTest.java
+++ src/test/java/com/feng/baseframework/test/StringTest.java
@@ -1,14 +1,11 @@
 package com.feng.baseframework.test;
 
-import com.feng.baseframework.util.DateUtil;
 import com.feng.baseframework.util.IPUtil;
 import org.apache.commons.lang.StringUtils;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
 /**
  * baseframework
@@ -43,20 +40,6 @@ public class StringTest {
         String endIp = "192.168.60.255";
         String ipStr = "192.168.60.14";
         System.out.println(IPUtil.ipExistsInRange(ipStr,startIp,endIp));
-    }
-
-    @Test
-    public void PatternTest(){
-        String reg = "([2]\\d{3}(((0[13578]|1[02])([0-2]\\d|3[01]))|((0[469]|11)([0-2]\\d|30))|(02([01]\\d|2[0-8])))([01][0-9]|2[0-3])([0-5]\\d)([0-5]\\d))";
-        String str = "9003268435225620181029090237606509";
-        Pattern pattern = Pattern.compile (reg);
-        Matcher matcher = pattern.matcher (str);
-        while (matcher.find ())
-        {
-            System.out.println (matcher.group ());
-        }
-
-		System.out.println(DateUtil.getDateByPattern(str));
     }
 
 }
