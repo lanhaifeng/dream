0e0a61287d269d082626a9288abbec96c4eb987e
lanhaifeng <lanhaifeng@mchz.com.cn>
2019-03-01 14:35:28 +0800

    restTemplate支持https

--- src/test/java/com/feng/baseframework/test/RestTemplateTest.java
+++ src/test/java/com/feng/baseframework/test/RestTemplateTest.java
@@ -1,8 +1,12 @@
 package com.feng.baseframework.test;
 
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.*;
 import org.springframework.web.client.RestTemplate;
+
+import javax.annotation.Resource;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * @ProjectName: baseframework
@@ -15,12 +19,40 @@ import org.springframework.web.client.RestTemplate;
  * @Version: 1.0
  */
 public class RestTemplateTest extends BaseFrameworkApplicationTest {
-    @Autowired
+
+    @Resource(name = "sshRestTemplate")
     private RestTemplate restTemplate;
 
     @Test
     public void testRestTemplateGet(){
         String html = restTemplate.getForObject("http://www.baidu.com",String.class);
         System.out.println(html);
+    }
+
+    @Test
+    public void testRestTemplateHttps() {
+        HttpHeaders requestHeaders = new HttpHeaders();
+        requestHeaders.setContentType(MediaType.APPLICATION_JSON_UTF8);
+        String sessionId = "2462CE3D190389EF832184310C61E377";
+        List<String> cookies = new ArrayList<>();
+        cookies.add("JSESSIONID=" + sessionId);
+
+        requestHeaders.put(HttpHeaders.COOKIE,cookies);
+
+        HttpEntity<String> requestEntity = null;
+        String targetUrl = "https://192.168.230.206/capaa/searchBiz/ruleFactor/0/9003139968276064000_1551344443850201";
+        HttpMethod httpMethod = HttpMethod.GET;
+        String body = "{}";
+        switch (httpMethod){
+            case GET:
+                requestEntity = new HttpEntity<String>(requestHeaders);
+                break;
+            case POST:
+                requestEntity = new HttpEntity<String>(body, requestHeaders);
+                break;
+        }
+
+        ResponseEntity<String> response = restTemplate.exchange(targetUrl, httpMethod,requestEntity,String.class);
+        System.out.println(response.getBody());
     }
 }
