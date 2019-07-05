870e9e9e8f6a9b605571e215345f42397e54c97b
lanhaifeng <lanhaifeng@mchz.com.cn>
2019-07-05 10:37:49 +0800

    add domain validate support

---
+++ src/main/java/com/feng/baseframework/model/EntityBase.java
@@ -0,0 +1,72 @@
+package com.feng.baseframework.model;
+
+import com.feng.baseframework.util.ValidateUtils;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.StringUtils;
+
+import java.util.Collection;
+import java.util.List;
+import java.util.function.Predicate;
+
+/**
+ * license-spring-boot
+ * 2019/4/2 9:45
+ *
+ * @author lanhaifeng
+ * @since
+ **/
+@Slf4j
+public abstract class EntityBase {
+
+	/**
+	 * 当集合为空，返回true
+	 * @param messageList
+	 * @return
+	 */
+	private boolean emptyList(List<String> messageList) {
+		Predicate<Collection> empty = collection -> collection == null || collection.isEmpty();
+		return empty.test(messageList);
+	}
+
+	/**
+	 * 返回验证bean失败的前置描述
+	 * @return
+	 */
+	public abstract String preErrorDesc();
+
+	public abstract List<String> customValidate(Class groupCls);
+
+	/**
+	 * 验证当前实例是否合法
+	 * @return
+	 */
+	public void validate(){
+		List<String> messageList = ValidateUtils.validate(this);
+		List<String> customMessageList = customValidate(null);
+		if(!emptyList(customMessageList)){
+			messageList.addAll(customMessageList);
+		}
+
+		if(!emptyList(messageList)){
+			log.error(String.format("[%s] bean validate fail:", preErrorDesc()) + StringUtils.join(messageList, ","));
+			throw new IllegalArgumentException("bean validate fail");
+		}
+	}
+
+	/**
+	 * 验证当前实例是否合法
+	 * @param groupCls 分组
+	 * @return
+	 */
+	public void validate(Class groupCls){
+		List<String> messageList = ValidateUtils.validate(this, groupCls);
+		List<String> customMessageList = customValidate(groupCls);
+		if(!emptyList(messageList)){
+			messageList.addAll(customMessageList);
+		}
+		if(!emptyList(messageList)){
+			log.error(String.format("{} bean validate fail:", preErrorDesc()) + StringUtils.join(messageList, ","));
+			throw new IllegalArgumentException("bean validate fail");
+		}
+	}
+}
---
+++ src/main/java/com/feng/baseframework/util/ValidateUtils.java
@@ -0,0 +1,73 @@
+package com.feng.baseframework.util;
+
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+import java.util.*;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * 2019/04/02 16:11
+ * 验证工具类
+ *
+ * @author lanhaifeng
+ * @since
+ **/
+public class ValidateUtils {
+	private static ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+
+	public static <T> List<String> validate(T t) {
+		Validator validator = factory.getValidator();
+		Set<ConstraintViolation<T>> constraintViolations = validator.validate(t);
+
+		List<String> messageList = new ArrayList<>();
+		for (ConstraintViolation<T> constraintViolation : constraintViolations) {
+			messageList.add(constraintViolation.getMessage());
+		}
+		return messageList;
+	}
+
+	public static <T> List<String> validate(T t, Class groupCls) {
+		Validator validator = factory.getValidator();
+		Set<ConstraintViolation<T>> constraintViolations = validator.validate(t, groupCls);
+		constraintViolations = Optional.ofNullable(constraintViolations).orElse(new HashSet<>());
+		List<String> messageList = new ArrayList<>();
+		for (ConstraintViolation<T> constraintViolation : constraintViolations) {
+			messageList.add(constraintViolation.getMessage());
+		}
+		return messageList;
+	}
+
+
+	public static Boolean validateRegex(String regex, String val){
+		return match(regex, val);
+	}
+
+	public static Boolean validateString(String val){
+		return validateRegex(V_STRING, val);
+	}
+
+	/**
+	 * @param regex 正则表达式字符串
+	 * @param str 要匹配的字符串
+	 * @return 如果str 符合 regex的正则表达式格式,返回true, 否则返回 false;
+	 */
+	private static boolean match(String regex, String str)
+	{
+		Pattern pattern = Pattern.compile(regex);
+		Matcher matcher = pattern.matcher(str);
+		return matcher.matches();
+	}
+
+	//中英文、数字、-、_、.
+	public static final String V_STRING = "^(//s&;&;[^//f//n//r//t//v])*|([A-Za-z-_.0-9\\u4E00-\\u9FA5]+)$";
+	//手机
+	public static final String V_MOBILE="^(1)[0-9]{10}$";
+	//邮件
+	public static final String V_EMAIL="^\\w+((-\\w+)|(\\.\\w+))*\\@[A-Za-z0-9]+((\\.|-)[A-Za-z0-9]+)*\\.[A-Za-z0-9]+$";
+
+	public static final String V_IP_DOMAIN="^((([\\da-fA-F]{1,4}:){7}[\\da-fA-F]{1,4})|(((25[0-5]|2[0-4]\\d|[01]?\\d\\d?)\\.){3}(25[0-5]|2[0-4]\\d|[01]?\\d\\d?)))|([a-zA-Z0-9]([a-zA-Z0-9\\-]{0,61}[a-zA-Z0-9])?\\.)+[a-zA-Z]{2,6}$";
+
+}
