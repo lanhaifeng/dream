4cf3c926fc6b2a85b258f54b36c326853683a4a9
lanhaifeng <lanhaifeng@mchz.com.cn>
2019-07-26 11:07:59 +0800

    edit ehcache

--- src/main/java/com/feng/baseframework/util/EhcacheUtil.java
+++ src/main/java/com/feng/baseframework/util/EhcacheUtil.java
@@ -36,8 +36,41 @@ public class EhcacheUtil {
 	}
 
 	public void put(String cacheName, String key, Object value) {
+		put(cacheName, key, value, -1, -1, false);
+	}
+
+	public void putEternal(String cacheName, String key, Object value) {
+		put(cacheName, key, value, -1, -1, true);
+	}
+
+	public void put(String cacheName, String key, Object value, int timeout) {
+		put(cacheName, key, value, timeout, -1, false);
+	}
+
+	/**
+	 * put方法
+	 * @param cacheName  cache名
+	 * @param key		 缓存key
+	 * @param value		 缓存对象
+	 * @param timeToIdle 空闲过期时间，即最后一次访问后过期时间
+	 * @param timeToLive 存活时间，即创建后过期时间
+	 * @
+	 */
+	public void put(String cacheName, String key, Object value, int timeToIdle, int timeToLive, boolean eternal) {
 		Cache cache = manager.getCache(cacheName);
 		Element element = new Element(key, value);
+		if(!eternal){
+			if(timeToIdle >= 0){
+				element.setTimeToIdle(timeToIdle);
+			}
+
+			if(timeToLive >= 0){
+				element.setTimeToLive(timeToLive);
+			}
+		}else {
+			element.setEternal(eternal);
+		}
+
 		cache.put(element);
 	}
 
--- src/test/java/com/feng/baseframework/util/EhcacheUtilTest.java
+++ src/test/java/com/feng/baseframework/util/EhcacheUtilTest.java
@@ -1,13 +1,20 @@
 package com.feng.baseframework.util;
 
+import com.feng.baseframework.common.MockitoBaseTest;
 import io.jsonwebtoken.lang.Assert;
 import org.junit.Test;
 
-public class EhcacheUtilTest {
+public class EhcacheUtilTest extends MockitoBaseTest {
 
 	@Test
 	public void put() {
-		EhcacheUtil.getInstance().put(EhcacheUtil.cacheName, "test", "test");
+		EhcacheUtil.getInstance().putEternal(EhcacheUtil.cacheName, "test", "test");
+		Assert.state("test".equals(EhcacheUtil.getInstance().get(EhcacheUtil.cacheName, "test")));
+		try {
+			Thread.sleep(20000l);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
 		Assert.state("test".equals(EhcacheUtil.getInstance().get(EhcacheUtil.cacheName, "test")));
 	}
 
