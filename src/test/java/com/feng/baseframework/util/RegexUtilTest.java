b6fb4460ba583591e667c7123a9bb595f35f2a41
lanhaifeng <lanhaifeng@mchz.com.cn>
2020-06-08 20:23:13 +0800

    正则测试

--- src/test/java/com/feng/baseframework/util/RegexUtilTest.java
+++ src/test/java/com/feng/baseframework/util/RegexUtilTest.java
@@ -1,5 +1,6 @@
 package com.feng.baseframework.util;
 
+import io.jsonwebtoken.lang.Assert;
 import org.junit.Test;
 
 public class RegexUtilTest {
@@ -8,6 +9,9 @@ public class RegexUtilTest {
 	public void regexSql() {
 		String regex = "SELECT COUNT.* FROM INFORMATION_SCHEMA.*";
 		String sqltext = "SELECT COUNT ( * ) FROM INFORMATION_SCHEMAtest";
-		System.out.println(RegexUtil.regexSql(regex, sqltext));
+		Assert.state(RegexUtil.regexSql(regex, sqltext));
+
+		regex = "SELECT COUNT \\( \\* \\) FROM INFORMATION_SCHEMA.*";
+		Assert.state(RegexUtil.regexSql(regex, sqltext));
 	}
 }
