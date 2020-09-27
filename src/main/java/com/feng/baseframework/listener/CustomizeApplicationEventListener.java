51b4c3b6c9345848a60f1b53a07fc49404ddab7a
lanhaifeng <lanhaifeng@mchz.com.cn>
2020-09-27 17:30:19 +0800

    spring监听器测试和容器启动发布自定义事件

--- src/main/java/com/feng/baseframework/listener/CustomizeApplicationEventListener.java
+++ src/main/java/com/feng/baseframework/listener/CustomizeApplicationEventListener.java
@@ -1,12 +1,23 @@
 package com.feng.baseframework.listener;
 
 import com.feng.baseframework.event.CustomizeApplicationEvent;
+import javafx.fxml.Initializable;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.InitializingBean;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
+import java.net.URL;
 import java.util.Optional;
+import java.util.ResourceBundle;
 
 /**
  * baseframework
@@ -17,14 +28,41 @@ import java.util.Optional;
  * @since
  **/
 @Component
-public class CustomizeApplicationEventListener implements ApplicationListener<CustomizeApplicationEvent> {
+public class CustomizeApplicationEventListener implements ApplicationContextAware,
+		ApplicationListener<CustomizeApplicationEvent>,
+		ApplicationRunner,
+		InitializingBean {
 
 	private static Logger logger = LoggerFactory.getLogger(CustomizeApplicationEventListener.class);
+	private ApplicationContext applicationContext;
 
 	@Override
 	public void onApplicationEvent(CustomizeApplicationEvent customizeApplicationEvent) {
 		Optional.ofNullable(customizeApplicationEvent).ifPresent(event -> {
 			logger.info("监听自定义事件:" + customizeApplicationEvent.getClass());
+			if(customizeApplicationEvent.getSource() instanceof String){
+				logger.info("事件源:" + customizeApplicationEvent.getSource());
+			}
 		});
+	}
+
+	@Override
+	public void run(ApplicationArguments applicationArguments) throws Exception {
+		applicationContext.publishEvent(new CustomizeApplicationEvent("ApplicationRunner run"));
+	}
+
+	@Override
+	public void afterPropertiesSet() throws Exception {
+		applicationContext.publishEvent(new CustomizeApplicationEvent("InitializingBean afterPropertiesSet"));
+	}
+
+	@PostConstruct
+	public void postConstruct(){
+		applicationContext.publishEvent(new CustomizeApplicationEvent("@PostConstruct"));
+	}
+
+	@Override
+	public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+		this.applicationContext = applicationContext;
 	}
 }
---
+++ src/main/java/com/feng/baseframework/listener/CustomizeDoApplicationListener.java
@@ -0,0 +1,25 @@
+package com.feng.baseframework.listener;
+
+import com.feng.baseframework.event.CustomizeApplicationEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
+
+/**
+ * baseframework
+ * 2020/9/27 16:56
+ * 通过监听器实现容器启动后执行方法
+ *
+ * @author lanhaifeng
+ * @since
+ **/
+@Component
+public class CustomizeDoApplicationListener implements ApplicationListener<ContextRefreshedEvent> {
+
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
+		if (contextRefreshedEvent.getApplicationContext().getParent() == null){
+			contextRefreshedEvent.getApplicationContext().publishEvent(new CustomizeApplicationEvent("ApplicationListener<ContextRefreshedEvent> onApplicationEvent"));
+		}
+	}
+}
