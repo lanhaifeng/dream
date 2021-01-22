81db02ecec2c82e54d507843ae0ad7fdfe1c804c
lanhaifeng <lanhaifeng@mchz.com.cn>
2021-01-22 11:10:31 +0800

    测试list ifRemove

--- src/test/java/com/feng/baseframework/jdk8/concurrent/CollectionTest.java
+++ src/test/java/com/feng/baseframework/jdk8/concurrent/CollectionTest.java
@@ -1,9 +1,13 @@
 package com.feng.baseframework.jdk8.concurrent;
 
+import com.feng.baseframework.model.Role;
+import io.jsonwebtoken.lang.Assert;
+import org.apache.commons.lang.StringUtils;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 /**
  * baseframework
@@ -16,11 +20,28 @@ import java.util.List;
 public class CollectionTest {
 
 	@Test
-	public void genericsCollection() {
-		List<Object> listObj = new ArrayList<>();
+	public void testIfRemove() {
+		List<Role> listObj = new ArrayList<>();
+		listObj.add(new Role(1, "test", "test"));
+		listObj.add(new Role(null, "test", "test"));
+		listObj.add(new Role(1, "test", "test"));
+		listObj.add(new Role(null, "test", "test"));
+
 
 		List<String> listStr = new ArrayList<>();
+		listStr.add(null);
+		listStr.add("test");
+		listStr.add("");
+		listStr.add("test1");
 
+		Assert.state(listObj.size() == 4);
+		listObj.removeIf(role -> Objects.isNull(role.getId()));
 
+		Assert.state(listObj.size() == 2);
+
+		Assert.state(listStr.size() == 4);
+		listStr.removeIf(StringUtils::isBlank);
+
+		Assert.state(listStr.size() == 2);
 	}
 }
